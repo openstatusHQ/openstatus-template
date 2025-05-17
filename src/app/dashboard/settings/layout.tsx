be03cf367f5faff13d9eca6428f8ed1610041440
Maximilian Kaske <maximilian@kaske.org>
2025-05-17 23:27:30 +0200

    fix:

--- src/app/dashboard/settings/layout.tsx
+++ src/app/dashboard/settings/layout.tsx
@@ -1,7 +1,6 @@
 import { AppHeader, AppHeaderContent } from "@/components/nav/app-header";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { NavBreadcrumb } from "@/components/nav/nav-breadcrumb";
-import { Cog, CreditCard, User } from "lucide-react";
+import { Breadcrumb } from "./breadcrumb";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,19 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AppHeader>
         <AppHeaderContent>
           <SidebarTrigger />
-          <NavBreadcrumb
-            items={[
-              { type: "page", label: "Settings" },
-              {
-                type: "select",
-                items: [
-                  { value: "general", label: "General", icon: Cog },
-                  { value: "billing", label: "Billing", icon: CreditCard },
-                  { value: "account", label: "Account", icon: User },
-                ],
-              },
-            ]}
-          />
+          <Breadcrumb />
         </AppHeaderContent>
       </AppHeader>
       <main className="flex-1 w-full">{children}</main>
