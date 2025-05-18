0beb791f8da77b2dd1ef24fe4af7d0b4cb937659
Maximilian Kaske <maximilian@kaske.org>
2025-05-18 23:15:04 +0200

    wip:

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -28,6 +28,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Copy, Ellipsis, Lock } from "lucide-react";
 
 export default function Page() {
@@ -69,7 +70,7 @@ export default function Page() {
           </FormCardContent>
           <FormCardFooter className="[&>:last-child]:ml-0">
             <FormCardFooterInfo>
-              Used when interacting with the API or Support.
+              Used when interacting with the API or for help on Discord.
             </FormCardFooterInfo>
           </FormCardFooter>
         </FormCard>
@@ -85,40 +86,48 @@ export default function Page() {
             <FormCardDescription>Manage your team members.</FormCardDescription>
           </FormCardHeader>
           <FormCardContent>
-            {/* TABS with members and pending invitations as table */}
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Name</TableHead>
-                  <TableHead>Email</TableHead>
-                  <TableHead>Role</TableHead>
-                  <TableHead>Created</TableHead>
-                  <TableHead>
-                    <span className="sr-only">Actions</span>
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Maximilian Kaske</TableCell>
-                  <TableCell>max@openstatus.dev</TableCell>
-                  <TableCell>Admin</TableCell>
-                  <TableCell>2021-01-01</TableCell>
-                  <TableCell>
-                    <div className="flex justify-end">
-                      <Button
-                        size="icon"
-                        variant="ghost"
-                        className="size-5"
-                        aria-label="Edit member"
-                      >
-                        <Ellipsis size={16} aria-hidden="true" />
-                      </Button>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+            <Tabs defaultValue="members">
+              <TabsList>
+                <TabsTrigger value="members">Members</TabsTrigger>
+                <TabsTrigger value="pending">Pending</TabsTrigger>
+              </TabsList>
+              <TabsContent value="members">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Name</TableHead>
+                      <TableHead>Email</TableHead>
+                      <TableHead>Role</TableHead>
+                      <TableHead>Created</TableHead>
+                      <TableHead>
+                        <span className="sr-only">Actions</span>
+                      </TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>Maximilian Kaske</TableCell>
+                      <TableCell>max@openstatus.dev</TableCell>
+                      <TableCell>Admin</TableCell>
+                      <TableCell>2021-01-01</TableCell>
+                      <TableCell>
+                        <div className="flex justify-end">
+                          <Button
+                            size="icon"
+                            variant="ghost"
+                            className="size-5"
+                            aria-label="Edit member"
+                          >
+                            <Ellipsis size={16} aria-hidden="true" />
+                          </Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TabsContent>
+              <TabsContent value="pending"></TabsContent>
+            </Tabs>
           </FormCardContent>
           <FormCardFooter>
             <FormCardFooterInfo>
