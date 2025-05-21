6f0243fe5393f01c7204eb7e85e7d1c3fd6a75bb
Maximilian Kaske <maximilian@kaske.org>
2025-05-21 10:44:30 +0200

    wip: form validation

--- src/components/forms/status-page/form-general.tsx
+++ src/components/forms/status-page/form-general.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { InputWithAddons } from "@/components/common/input-with-addons";
-import { FormDescription } from "@/components/ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormDescription,
+  FormMessage,
+  FormLabel,
+} from "@/components/ui/form";
 
 import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   FormCard,
   FormCardContent,
@@ -19,6 +26,8 @@ import {
 } from "../form-card";
 import { Button } from "@/components/ui/button";
 import { z } from "zod";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 const schema = z.object({
   title: z.string().min(1, "Title is required"),
@@ -30,10 +39,37 @@ const schema = z.object({
 type FormValues = z.infer<typeof schema>;
 
 export function FormGeneral() {
-  const form = useForm();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: "",
+      slug: "",
+      favicon: undefined,
+      description: "",
+    },
+  });
+  const [isPending, startTransition] = useTransition();
+
+  function submitAction(values: FormValues) {
+    if (isPending) return;
+
+    startTransition(async () => {
+      try {
+        const promise = new Promise((resolve) => setTimeout(resolve, 1000));
+        toast.promise(promise, {
+          loading: "Saving...",
+          success: () => JSON.stringify(values),
+          error: "Failed to save",
+        });
+        await promise;
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
   return (
     <Form {...form}>
-      <form>
+      <form onSubmit={form.handleSubmit(submitAction)}>
         <FormCard>
           <FormCardHeader>
             <FormCardTitle>General</FormCardTitle>
@@ -43,41 +79,82 @@ export function FormGeneral() {
           </FormCardHeader>
           <FormCardSeparator />
           <FormCardContent className="grid gap-4">
-            <div className="grid gap-1.5">
-              <Label>Title</Label>
-              <Input placeholder="My Status Page" />
-              <FormDescription>
-                Enter a descriptive name for your status page.
-              </FormDescription>
-            </div>
-            <div className="grid gap-1.5">
-              <Label>Slug</Label>
-              <InputWithAddons
-                placeholder="status"
-                trailing=".openstatus.dev"
-              />
-              <FormDescription>
-                Choose a unique subdomain for your status page (minimum 3
-                characters).
-              </FormDescription>
-            </div>
-            <div className="grid gap-1.5">
-              <Label>Favicon</Label>
-              <div className="flex flex-row items-center space-x-2">
-                <div className="size-[36px] bg-muted rounded-md border"></div>
-                <Input type="file" />
-              </div>
-            </div>
-            <div className="grid gap-1.5 col-span-full">
-              <Label>Description</Label>
-              <Textarea />
-              <FormDescription>
-                Provide a brief overview of your status page purpose.
-              </FormDescription>
-            </div>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="My Status Page" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>
+                    Enter a descriptive name for your status page.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="slug"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Slug</FormLabel>
+                  <FormControl>
+                    <InputWithAddons
+                      placeholder="status"
+                      trailing=".openstatus.dev"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>
+                    Choose a unique subdomain for your status page (minimum 3
+                    characters).
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="favicon"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Favicon</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-row items-center space-x-2">
+                      <div className="size-[36px] bg-muted rounded-md border"></div>
+                      <Input type="file" {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>
+                    Choose a unique subdomain for your status page (minimum 3
+                    characters).
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>
+                    Provide a brief overview of your status page purpose.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
           </FormCardContent>
           <FormCardFooter>
-            <Button>Submit</Button>
+            <Button type="submit">Submit</Button>
           </FormCardFooter>
         </FormCard>
       </form>
