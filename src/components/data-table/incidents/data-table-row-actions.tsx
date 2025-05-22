05cc8b87e4dee213789793a86a6a75cfe3685511
Maximilian Kaske <maximilian@kaske.org>
2025-05-22 15:13:32 +0200

    wip: incident actions

--- src/components/data-table/incidents/data-table-row-actions.tsx
+++ src/components/data-table/incidents/data-table-row-actions.tsx
@@ -1,16 +1,9 @@
 "use client";
 
 import { Row } from "@tanstack/react-table";
-import { Bookmark, Check, MoreHorizontal, Trash2 } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { getActions } from "@/data/incidents.client";
+import { QuickActions } from "@/components/dropdowns/quick-actions";
 
 interface DataTableRowActionsProps<TData> {
   row?: Row<TData>;
@@ -20,31 +13,15 @@ export function DataTableRowActions<TData>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _props: DataTableRowActionsProps<TData>
 ) {
+  const actions = getActions({});
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted ml-auto"
-        >
-          <MoreHorizontal />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>
-          <Bookmark />
-          Acknowledge
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Check />
-          Resolve
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem variant="destructive">
-          <Trash2 /> Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <QuickActions
+      actions={actions}
+      deleteAction={{
+        title: "Incident",
+        confirmationValue: "incident",
+      }}
+    />
   );
 }
