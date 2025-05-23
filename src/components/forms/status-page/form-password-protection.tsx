e1f2f765ced41841a3f64c873feeed3714d8d85f
Maximilian Kaske <maximilian@kaske.org>
2025-05-23 16:54:15 +0200

    chore: form password protection

--- src/components/forms/status-page/form-password-protection.tsx
+++ src/components/forms/status-page/form-password-protection.tsx
@@ -1,39 +1,45 @@
 "use client";
 
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
   FormCard,
+  FormCardContent,
+  FormCardDescription,
   FormCardFooter,
   FormCardFooterInfo,
   FormCardHeader,
   FormCardTitle,
-  FormCardDescription,
-  FormCardContent,
   FormCardUpgrade,
 } from "@/components/forms/form-card";
-
-import { Label } from "@/components/ui/label";
-
-import { InputWithAddons } from "@/components/common/input-with-addons";
-import { Lock } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Link } from "@/components/common/link";
 import { Button } from "@/components/ui/button";
+import { Lock } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import React, { useTransition } from "react";
+import { useTransition } from "react";
 import { toast } from "sonner";
-import { Link } from "@/components/common/link";
 
 const LOCKED = true;
 
 const schema = z.object({
-  domain: z.string().min(1, "Domain is required"),
+  passwordProtected: z.boolean().optional(),
+  password: z.string().optional(),
 });
 
 type FormValues = z.infer<typeof schema>;
 
-export function FormCustomDomain({
+export function FormPasswordProtection({
   defaultValues,
   onSubmit,
   ...props
@@ -41,13 +47,14 @@ export function FormCustomDomain({
   defaultValues?: FormValues;
   onSubmit?: (values: FormValues) => Promise<void> | void;
 }) {
+  const [isPending, startTransition] = useTransition();
   const form = useForm<FormValues>({
     resolver: zodResolver(schema),
     defaultValues: defaultValues ?? {
-      domain: undefined,
+      passwordProtected: false,
+      password: "",
     },
   });
-  const [isPending, startTransition] = useTransition();
 
   function submitAction(values: FormValues) {
     if (isPending) return;
@@ -74,24 +81,41 @@ export function FormCustomDomain({
         <FormCard>
           {LOCKED ? <FormCardUpgrade /> : null}
           <FormCardHeader>
-            <FormCardTitle>Custom Domain</FormCardTitle>
+            <FormCardTitle>Password Protection</FormCardTitle>
             <FormCardDescription>
-              Use your own domain for your status page.
+              Protect your status page with a password.
             </FormCardDescription>
           </FormCardHeader>
-          <FormCardContent>
+          <FormCardContent className="grid gap-4">
             <FormField
               control={form.control}
-              name="domain"
+              name="passwordProtected"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1">
+                    <FormLabel>Enable Password Protection</FormLabel>
+                    <FormDescription>
+                      Hide the page from the public
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
               render={({ field }) => (
                 <FormItem>
-                  <Label>Domain</Label>
-                  <InputWithAddons
-                    placeholder="status.openstatus.dev"
-                    leading="https://"
-                    disabled={LOCKED}
-                    {...field}
-                  />
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
@@ -99,7 +123,7 @@ export function FormCustomDomain({
           </FormCardContent>
           <FormCardFooter>
             <FormCardFooterInfo>
-              Learn more about <Link href="#">Custom Domain</Link>.
+              Learn more about <Link href="#">Password Protection</Link>.
             </FormCardFooterInfo>
             {LOCKED ? (
               <Button type="button" asChild>
--- src/components/forms/status-page/update.tsx
+++ src/components/forms/status-page/update.tsx
@@ -1,73 +1,24 @@
-"use client";
-
 import {
   FormCard,
-  FormCardContent,
   FormCardDescription,
   FormCardFooter,
-  FormCardFooterInfo,
   FormCardGroup,
   FormCardHeader,
   FormCardTitle,
-  FormCardUpgrade,
 } from "@/components/forms/form-card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Link } from "@/components/common/link";
-import { Form, FormDescription } from "@/components/ui/form";
-import { useForm } from "react-hook-form";
 import { FormAlertDialog } from "@/components/forms/form-alert-dialog";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Lock } from "lucide-react";
 import { FormGeneral } from "./form-general";
 import { FormMonitors } from "./form-monitors";
 import { FormCustomDomain } from "./form-custom-domain";
+import { FormPasswordProtection } from "./form-password-protection";
 
 export function FormStatusPageUpdate() {
-  const form = useForm();
   return (
     <FormCardGroup>
       <FormGeneral />
       <FormMonitors />
       <FormCustomDomain />
-      <FormCard>
-        <FormCardUpgrade />
-        <FormCardHeader>
-          <FormCardTitle>Password Protection</FormCardTitle>
-          <FormCardDescription>
-            Protect your status page with a password.
-          </FormCardDescription>
-        </FormCardHeader>
-        <FormCardContent>
-          <Form {...form}>
-            <form className="grid gap-4">
-              <div className="flex flex-row items-start space-x-3 space-y-0">
-                <Checkbox />
-                <div className="space-y-1 leading-none">
-                  <Label>Enable Password Protection</Label>
-                  <FormDescription>
-                    Hide the page from the public
-                  </FormDescription>
-                </div>
-              </div>
-              <div className="grid gap-1.5">
-                <Label>Password</Label>
-                <Input />
-              </div>
-            </form>
-          </Form>
-        </FormCardContent>
-        <FormCardFooter>
-          <FormCardFooterInfo>
-            Learn more about <Link href="#">Password Protection</Link>.
-          </FormCardFooterInfo>
-          <Button>
-            <Lock />
-            Upgrade
-          </Button>
-        </FormCardFooter>
-      </FormCard>
+      <FormPasswordProtection />
       <FormCard variant="destructive">
         <FormCardHeader>
           <FormCardTitle>Danger Zone</FormCardTitle>
