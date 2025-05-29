40037ac9163059f7b9ddc2656d53e2fcf7365ff5
Maximilian Kaske <maximilian@kaske.org>
2025-05-29 12:45:40 +0200

    chore: hide right sidebar by default

--- src/app/dashboard/status-pages/(view)/status-reports/layout.tsx
+++ src/app/dashboard/status-pages/(view)/status-reports/layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={false}>
       <div className="flex-1 w-full">{children}</div>
       <div className="hidden lg:block">
         <Sidebar />
