5bad572da675df976e9036960a053c3c8c6c8ef8
Maximilian Kaske <maximilian@kaske.org>
2025-08-07 14:07:28 +0200

    feat: add github quick link floating button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,7 @@
 import { AppSidebar } from "@/components/nav/app-sidebar";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -6,6 +9,25 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>{children}</SidebarInset>
+      <FloatingButton />
     </SidebarProvider>
+  );
+}
+
+function FloatingButton() {
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      <Button asChild className="group" size="sm">
+        <a
+          href="https://github.com/openstatusHQ/data-table-filters"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="mr-1">GitHub</span>
+          <ArrowRight className="relative mb-[1px] inline h-4 w-0 transition-all group-hover:w-4" />
+          <ChevronRight className="relative mb-[1px] inline h-4 w-4 transition-all group-hover:w-0" />
+        </a>
+      </Button>
+    </div>
   );
 }
