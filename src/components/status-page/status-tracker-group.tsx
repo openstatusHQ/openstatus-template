dad2b408d82f27f5913f77d77b121090d18feb4c
Maximilian Kaske <maximilian@kaske.org>
2025-08-16 13:59:51 +0200

    wip: group

--- src/components/status-page/status-tracker-group.tsx
+++ src/components/status-page/status-tracker-group.tsx
@@ -18,11 +18,16 @@ export function StatusTrackerGroup({
   variant?: VariantType;
 }) {
   return (
-    <Collapsible className="flex flex-col gap-2">
+    <Collapsible
+      className={cn(
+        "-mx-3",
+        "hover:bg-muted/50 data-[state=open]:bg-muted/50 rounded-lg border border-transparent hover:border-border/50 data-[state=open]:border-border/50",
+        className
+      )}
+    >
       <CollapsibleTrigger
         className={cn(
-          "group/monitor w-full flex items-center justify-between gap-2 font-medium border border-transparent",
-          "hover:bg-muted/50 data-[state=open]:bg-muted/50 rounded-lg px-3 py-2",
+          "group/monitor w-full px-3 py-2 flex items-center justify-between gap-2 font-medium border border-transparent",
           "cursor-pointer",
           className
         )}
@@ -31,13 +36,13 @@ export function StatusTrackerGroup({
       >
         {title}
         <div className="flex items-center gap-2">
+          <StatusMonitorStatus className="text-sm" />
           <StatusMonitorIcon />
-          <StatusMonitorStatus />
         </div>
       </CollapsibleTrigger>
       <CollapsibleContent
         className={cn(
-          "flex flex-col gap-3",
+          "flex flex-col gap-3 px-3 py-2 border-t border-border/50",
           "overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down"
         )}
       >
