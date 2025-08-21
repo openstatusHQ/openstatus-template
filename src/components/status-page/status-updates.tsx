035dd4b730f16ee39c275510b054279bdbd9f4e4
Maximilian Kaske <maximilian@kaske.org>
2025-08-21 08:00:38 +0200

    wip: status update

--- src/components/status-page/status-updates.tsx
+++ src/components/status-page/status-updates.tsx
@@ -5,6 +5,9 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "../ui/input";
+
 export function StatusUpdates({
   className,
   ...props
@@ -21,7 +24,39 @@ export function StatusUpdates({
           Get updates
         </Button>
       </PopoverTrigger>
-      <PopoverContent align="end">Subscribe to status updates.</PopoverContent>
+      <PopoverContent align="end" className="p-0 overflow-hidden">
+        <Tabs defaultValue="email">
+          <TabsList className="w-full rounded-none border-b">
+            <TabsTrigger value="email">Email</TabsTrigger>
+            <TabsTrigger value="rss">RSS</TabsTrigger>
+            <TabsTrigger value="atom">Atom</TabsTrigger>
+          </TabsList>
+          <TabsContent value="email" className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 px-2 pb-2 border-b">
+              <p className="text-sm text-foreground">
+                Get email notifications whenever a report has been created or
+                resolved
+              </p>
+              <Input placeholder="notify@me.com" />
+            </div>
+            <div className="px-2 pb-2">
+              <Button className="w-full">Subscribe</Button>
+            </div>
+          </TabsContent>
+          <TabsContent value="rss" className="px-2 pb-2">
+            <Input
+              placeholder="https://status.openstatus.dev/feed/rss"
+              disabled
+            />
+          </TabsContent>
+          <TabsContent value="atom" className="px-2 pb-2">
+            <Input
+              placeholder="https://status.openstatus.dev/feed/atom"
+              disabled
+            />
+          </TabsContent>
+        </Tabs>
+      </PopoverContent>
     </Popover>
   );
 }
