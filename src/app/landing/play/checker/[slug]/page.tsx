e0b584867e6dd48c6ae9b72b81df24aa14f40f0b
Maximilian Kaske <maximilian@kaske.org>
2025-11-16 18:13:20 +0100

    wip:

--- src/app/landing/play/checker/[slug]/page.tsx
+++ src/app/landing/play/checker/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getToolsPage } from "@/content/utils";
 import { Table } from "./client";
+import { CustomMDX } from "@/content/mdx";
 
 // just random to have one
 export async function generateStaticParams() {
@@ -12,18 +13,14 @@ export default async function Page({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  const page = getToolsPage("checker");
+  const page = getToolsPage("checker-slug");
   console.log({ slug });
   return (
     <section className="prose">
       <h1>{page.metadata.title}</h1>
       <p className="text-lg">{page.metadata.summary}</p>
       <Table />
-      <p>
-        The data will be stored for 7 days. If you want to persist the data,
-        login to your account.
-      </p>
-      {/* <CustomMDX source={page.content} /> */}
+      <CustomMDX source={page.content} />
     </section>
   );
 }
