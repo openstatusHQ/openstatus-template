db43595732da3f7d7cc2895252699d7adb801489
Maximilian Kaske <maximilian@kaske.org>
2025-11-19 20:23:45 +0100

    wip: sub nav

--- src/app/landing/layout.tsx
+++ src/app/landing/layout.tsx
@@ -1,15 +1,13 @@
 import { Footer } from "@/content/footer";
 import { Header } from "@/content/header";
-import { CopyLinkButton } from "./copy-link-button";
+import { SubNav } from "@/content/sub-nav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col gap-4 mx-auto max-w-5xl font-mono">
       <Header />
-      <main className="relative flex-1 px-4 py-8">
-        <CopyLinkButton className="absolute top-0 right-4" />
-        {children}
-      </main>
+      <SubNav />
+      <main className="flex-1 px-4">{children}</main>
       <Footer />
     </div>
   );
