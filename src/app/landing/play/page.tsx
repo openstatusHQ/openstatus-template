f1cba16ced59db7e97afa45822633a0aadd7e0b9
Maximilian Kaske <maximilian@kaske.org>
2025-11-21 13:00:40 +0100

    chore: update play description

--- src/app/landing/play/page.tsx
+++ src/app/landing/play/page.tsx
@@ -1,17 +1,17 @@
 import { components } from "@/content/mdx";
-import { playSection } from "@/data/content";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <section className="prose">
-      <h1>{playSection.label}</h1>
+      <h1>Playground</h1>
       <components.Grid cols={2}>
-        {playSection.items.map((tool) => (
+        {PLAY.map((tool) => (
           <div key={tool.href}>
             <p>
               <strong>{tool.label}</strong>
             </p>
+            <p className="text-muted-foreground">{tool.description}</p>
             <Link href={tool.href}>
               {tool.href
                 .replace(/^https:\/\/(www\.)?/, "")
@@ -23,3 +23,51 @@ export default function Page() {
     </section>
   );
 }
+
+const PLAY = [
+  {
+    label: "Global Speed Checker",
+    description: "Test the latency of your website worldwide",
+    href: "/landing/play/checker",
+  },
+  {
+    label: "cURL Builder",
+    description: "Curl your website",
+    href: "/landing/play/curl",
+  },
+  {
+    label: "Uptime SLA Calculator",
+    description: "Calculate downtime duration or uptime percentage",
+    href: "/landing/play/uptime-sla",
+  },
+  {
+    label: "Theme Explorer",
+    description: "Explore themes for your status page",
+    href: "https://themes.openstatus.dev",
+  },
+  {
+    label: "All Status Codes",
+    description: "Explore all HTTP status codes",
+    href: "https://openstat.us",
+  },
+  {
+    label: "Vercel Edge Ping",
+    description: "Use edge functions to ping your website",
+    href: "https://light.openstatus.dev",
+  },
+  {
+    label: "React Data Table",
+    description: "Shadcn tanstack table with nuqs integration",
+    href: "https://logs.run",
+  },
+  {
+    label: "Shadcn Time Picker",
+    description: "Shadcn time picker with date-fns integration",
+    href: "https://time.openstatus.dev",
+  },
+  {
+    label: "Astro Status Page",
+    description: "Astro status page with openstatus integration",
+    href: "https://astro.openstat.us",
+  },
+];
